1bbbe90c24808b0f626c9db48c8ced353fc854fc
Kevin Upton <k_upton@live.com>
2017-09-23 14:45:34 +1000

    added npm package

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-const npm = require('./package.json');
+const npm = require('../package.json');
 const cmd = require('node-cmd');
 const writeFile = require('write');
 const ProgressBar = require('progress');
 
-let total = 0;
 let completed = 0;
 
-const bar = new ProgressBar(':bar', { total: 10 });
+const keys  = ['dependencies', 'devDependencies'];
+const total = keys.reduce((value, key) => Object.keys(npm[key]).length + value, 0);
+const bar   = new ProgressBar(':bar', { total: total + 3 });
 
-['dependencies', 'devDependencies'].forEach(dep => {
+
+keys.forEach(dep => {
+  bar.tick();
+
   Object.keys(npm[dep]).sort().forEach(key => {
-    total++;
 
     cmd.get('npm show ' + key + ' version', function (err, data, stderr) {
       npm[dep][key] = data.replace('\n', '').trim();
       completed++;
 
-      bar.tick({
-        ':total': total + 1
-      });
+      bar.tick();
 
       if (completed === total) writeToFile();
     });
@@ -30,6 +31,6 @@ function writeToFile () {
   writeFile('./package.json', JSON.stringify(npm, null, 4))
     .then(() => {
       bar.tick();
-      console.log('Successfully removed carrots...');
+      console.log('\nSuccessfully removed carrots...');
     });
 }
