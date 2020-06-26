879fef5fab43bac02365619786ef00ebb44269fb
Kevin Upton <kevin@Mac-mini.local>
2020-06-26 15:27:05 +1000

    updated the carrot remover

--- src/index.js
+++ src/index.js
@@ -3,33 +3,43 @@ const npm = require(path.resolve('package.json'));
 const cmd = require('node-cmd');
 const writeFile = require('write');
 const ProgressBar = require('progress');
+const fs = require('fs');
 
 let completed = 0;
 
-const keys  = ['dependencies', 'devDependencies'];
+const keys = ['dependencies', 'devDependencies'];
 const total = keys.reduce((value, key) => Object.keys(npm[key]).length + value, 0);
-const bar   = new ProgressBar(':bar', { total: total + 3 });
+const bar = new ProgressBar(':bar', {total: total + 3});
 
-keys.forEach(dep => {
-  bar.tick();
+const isYarn = fs.existsSync(path.join(process.cwd(), 'yarn.lock'));
 
-  Object.keys(npm[dep]).sort().forEach(key => {
+cmd.get((isYarn ? 'yarn' : 'npm') + ' list --depth 0', function (err, data, stderr) {
+    keys.forEach(dep => {
+        bar.tick();
 
-    cmd.get('npm show ' + key + ' version', function (err, data, stderr) {
-      npm[dep][key] = data.replace('\n', '').trim();
-      completed++;
+        Object.keys(npm[dep]).sort().forEach(key => {
 
-      bar.tick();
+            const pattern = new RegExp('[├└]─ ' + key + '@([0-9\\.]+)');
+            const matches = pattern.exec(data);
 
-      if (completed === total) writeToFile();
+            if (!matches) {
+                console.warn('Could not find', key);
+                return;
+            }
+
+            npm[dep][key] = matches[1].trim();
+            completed++;
+
+            bar.tick();
+            if (completed === total) writeToFile();
+        });
     });
-  });
 });
 
-function writeToFile () {
-  writeFile('./package.json', JSON.stringify(npm, null, 2))
-    .then(() => {
-      bar.tick();
-      console.log('\nSuccessfully removed carrots...');
-    });
+function writeToFile() {
+    writeFile('./package.json', JSON.stringify(npm, null, 2))
+        .then(() => {
+            bar.tick();
+            console.log('\nSuccessfully removed carrots...');
+        });
 }
